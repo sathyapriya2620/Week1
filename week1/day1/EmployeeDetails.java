bacb752d28b69ac7bc532529ba07f4166d595dbc
sathyapriya2620 <43267603+sathyapriya2620@users.noreply.github.com>
2025-12-05 15:06:41 +0530

    Add files via upload

---
+++ week1/day1/EmployeeDetails.java
@@ -0,0 +1,32 @@
+package week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String name="John doe";
+		int age=30;
+		Float salary= 55000.75f;
+		char gender='M';
+		boolean acceptTerms=true;
+		float rating=4.5f;
+		long mobileNumber=9876543210l;
+		int pinCode=56001;
+		int luckyNumber=7;
+		
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(salary);
+		System.out.println(gender);
+		System.out.println(acceptTerms);
+		System.out.println(rating);
+		System.out.println(mobileNumber);
+		System.out.println(pinCode);
+		System.out.println(luckyNumber);
+		
+		
+
+	}
+
+}
